fef2ecdc675453773c1b5126db4e58866ab09220
Daniele Olmisani <1510799+mad4j@users.noreply.github.com>
2024-12-03 19:17:36 +0000

    Added day03 part2 Solution

--- src/bin/2024-day03.rs
+++ src/bin/2024-day03.rs
@@ -1,5 +1,7 @@
 use regex::Regex;
 
+
+
 const INPUT_STR: &str = include_str!("../../inputs/2024-day03.txt");
 
 fn solve_part1(input: &str) -> u32 {
@@ -21,7 +23,36 @@ fn solve_part1(input: &str) -> u32 {
 
 
 fn solve_part2(input: &str) -> u32 {
-    0
+
+    let re = Regex::new(r"(?<mul>mul)\((?<a>\d+),(?<b>\d+)\)|(?<do>do)\(\)|(?<dont>don\'t)\(\)").unwrap();
+
+    let mut result = 0;
+    let mut is_enabled = true;
+
+    for c in re.captures_iter(input).into_iter().collect::<Vec<_>>() {
+
+        //println!("{:?}", c);
+
+        if c.name("mul").is_some() {
+
+            if is_enabled {
+                let a: u32 = c.name("a").unwrap().as_str().parse().unwrap();
+                let b: u32 = c.name("b").unwrap().as_str().parse().unwrap();
+        
+                result += a*b;
+            }
+        }
+
+        if c.name("do").is_some() {
+            is_enabled = true;
+        }
+
+        if c.name("dont").is_some() {
+            is_enabled = false;
+        }
+    }
+
+    result
 }
 
 fn main() {
